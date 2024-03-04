3b3df8ad01129b92b9a3e23765c1b34f19dec138
유승완 <swan1032@naver.com>
2024-03-04 21:57:50 +0900

    feat: ConfigModule을 통한 env파일 불러오기 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [BookModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    BookModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
